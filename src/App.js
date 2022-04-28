9e1a39cf27e82ac30a0f25a9487c836194aff276
gavinlasher <Krgavin21@yahoo.com>
2022-04-27 22:58:49 -0600

    pokemontest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import Pokemon from "./Pokemon";
 
 function App() {
+const APP_ID = '2e269186';
+const APP_KEY = '84c174e6863d93a25b2e75e137d559a9'
+const example = `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}&type=public`
+
+const [pokemon, setPokemon] = useState([]);
+
+
+useEffect(()=>{
+  getRecipes();
+},[])
+
+const getRecipes = async () => {
+  const res = await fetch(
+   'https://pokeapi.co/api/v2/pokemon'
+  )
+  const data = await res.json();
+  console.log(data.results)
+  setPokemon(data.results)
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{pokemon.map(pokemon => (
+<Pokemon name={pokemon.name} key={pokemon.name}/>
+))}
     </div>
   );
 }
 
 export default App;
+// image={pokemon.sprites.other.dream_world.front_default}
---
+++ src/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pokemon = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src='' alt="" />
+    </div>
+  )
+
+}
+export default Pokemon
