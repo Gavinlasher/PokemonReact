df22f3836130d2cba494b7deaaaee238bff09f24
gavinlasher <Krgavin21@yahoo.com>
2022-04-28 17:15:38 -0600

    start

--- src/App.js
+++ src/App.js
@@ -11,27 +11,43 @@ const [pokemon, setPokemon] = useState([]);
 
 
 useEffect(()=>{
-  getRecipes();
+  getPokemon();
 },[])
 
-const getRecipes = async () => {
+const getPokemon = async () => {
   const res = await fetch(
-   'https://pokeapi.co/api/v2/pokemon'
+   'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=120'
   )
   const data = await res.json();
-  console.log(data.results)
+  console.log(data)
   setPokemon(data.results)
 };
 
 
   return (
-    <div className="App">
+  
+  
+    <div className="row">
+      <div className="col-md-12 bg-danger d-flex">
+        <img  className="img-fluid pp ms-2 p-2" src="https://cdn.pixabay.com/photo/2019/11/27/14/06/pokemon-4657023_1280.png" alt="" />
+        <h1 className="text-light  mt-3 ms-3 text-center">Pokemon</h1>
+      </div>
+    
+   
+<div className="row p-3">
+
 {pokemon.map(pokemon => (
-<Pokemon name={pokemon.name} key={pokemon.name}/>
-))}
-    </div>
+  <Pokemon name={pokemon.name} url={pokemon.url} pokeNumber={pokemon.url.split('/')[pokemon.url.split('/').length-2]} key={pokemon.name}/>
+  ))}
+  </div>
+    
+  </div>
+     
+   
+  
   );
 }
 
 export default App;
 // image={pokemon.sprites.other.dream_world.front_default}
+
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-const Pokemon = ({ name }) => {
+
+
+
+
+
+const Pokemon = ({ name, url, pokeNumber }) => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <img src='' alt="" />
+    <div className="col-md-3 selectable d-flex ">
+      <div className="bg-light shadow rounded p-2 mt-2 ">
+
+        <h1 className="text-dark">{name}</h1>
+        <img className="img-fluid p-3 pimg" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeNumber}.svg`} alt='' />
+      </div>
     </div>
   )
 
